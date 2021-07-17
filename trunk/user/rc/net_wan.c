3cb0e91b21eef16ebde2947ed235ac7611cd332a
chenxudong2020 <872603935@qq.com>
2021-07-17 13:59:15 +0800

    Update net_wan.c
    修改my.router

--- trunk/user/rc/net_wan.c
+++ trunk/user/rc/net_wan.c
@@ -1794,7 +1794,7 @@ update_hosts_router(const char *lan_ipaddr)
 			fprintf(fp, "%s %s.%s %s\n", lan_ipaddr, lan_hname, lan_dname, lan_hname);
 		else
 			fprintf(fp, "%s %s\n", lan_ipaddr, lan_hname);
-		fprintf(fp, "%s %s\n", lan_ipaddr, "my.router");
+		fprintf(fp, "%s %s\n", lan_ipaddr, "p.to");
 		
 		/* load static DHCP list */
 		load_user_config(fp, "/tmp", "hosts.static", NULL);
@@ -1809,7 +1809,7 @@ update_hosts_router(const char *lan_ipaddr)
 					fprintf(fp, "%s %s.%s %s\n", lan_addr6_host, lan_hname, lan_dname, lan_hname);
 				else
 					fprintf(fp, "%s %s\n", lan_addr6_host, lan_hname);
-				fprintf(fp, "%s %s\n", lan_addr6_host, "my.router");
+				fprintf(fp, "%s %s\n", lan_addr6_host, "p.to");
 			}
 		}
 #endif
